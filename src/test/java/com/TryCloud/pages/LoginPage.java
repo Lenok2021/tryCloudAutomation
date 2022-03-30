71ab421d8bbb286061fd26923923e97038260d78
pansherisherali <pansherisherali@gmail.com>
2022-03-30 12:36:35 -0500

    created login method

--- src/test/java/com/TryCloud/pages/LoginPage.java
+++ src/test/java/com/TryCloud/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package com.TryCloud.pages;
 
+import com.TryCloud.utilities.ConfigurationReader;
 import com.TryCloud.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -17,6 +18,13 @@ public class LoginPage extends BasePage{
 
     @FindBy(xpath = "//input[@id = 'submit-form']")
     public WebElement loginButton ;
+
+    public void login(){
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        userNameInput.sendKeys(ConfigurationReader.getProperty("username_1"));
+        userPasswordInput.sendKeys(ConfigurationReader.getProperty("password"));
+        loginButton.click();
+    }
 
 
 
