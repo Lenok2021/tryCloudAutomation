209022ec90634cab0ae175f28d797b7fefcc1f6c
cross <crosstown11223@yahoo.com>
2022-04-03 13:52:27 -0400

    completed US1_Login_Step_Defs

--- src/test/java/com/TryCloud/step_definitions/US1_Login_Step_Defs.java
+++ src/test/java/com/TryCloud/step_definitions/US1_Login_Step_Defs.java
@@ -22,42 +22,41 @@ public class US1_Login_Step_Defs {
         Driver.getDriver().get(ConfigurationReader.getProperty("env"));
 
     }
+
     @When("user enter valid {string} and {string}")
     public void user_enter_valid_and(String username, String password) {
 
-        loginPage.login(username,password);
+        loginPage.login(username, password);
     }
 
     @When("user click login button")
     public void user_click_login_button() {
 
         loginPage.loginButton.click();
-        BrowserUtils.sleep(2);
+
     }
 
     @Then("user should be at dashboard page")
     public void user_should_be_at_dashboard_page() {
 
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("dashboard"));
-
-
     }
 
     //login with invalid credentials
 
     @When("user enter invalid {string} and {string}")
     public void user_enter_invalid_and(String username, String password) {
-        loginPage.login(username,password);
+        loginPage.login(username, password);
+
     }
+
     @Then("{string} message should be displayed")
     public void message_should_be_displayed(String expectedMessage) {
-
-        Assert.assertEquals(expectedMessage,loginPage.warningMessage.getText());
-
+        Assert.assertEquals(expectedMessage, loginPage.warningMessage.getText());
 
     }
 
-    }
+}
 
 
 
